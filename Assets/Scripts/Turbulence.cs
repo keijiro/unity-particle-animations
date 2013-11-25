21f2700defb9099aaf52d3ac00c0b4b95c1df54a
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-11-25 21:04:35 +0900

    Added turbulence.

---
+++ Assets/Scripts/Turbulence.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turbulence : MonoBehaviour
+{
+    ParticleSystem.Particle[] particles;
+
+    void Start ()
+    {
+        particles = new ParticleSystem.Particle[particleSystem.maxParticles];
+    }
+
+    void Update ()
+    {
+        var number = particleSystem.GetParticles (particles);
+
+        particleSystem.SetParticles (particles, number);
+    }
+}
