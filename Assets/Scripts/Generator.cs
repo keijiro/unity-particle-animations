57ca2b422493eeecfb8ddf93d224e179ff82d666
Keijiro Takahashi <keijiro@MSX.local>
2013-11-27 08:35:01 +0900

    Added Rigidbodies.

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject prefab;
+    public float velocity = 5.0f;
+    public float rate = 20.0f;
+    public float radius = 2.0f;
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        while (timer > 0)
+        {
+            var position = transform.position + Random.insideUnitSphere * radius;
+            var go = Instantiate (prefab, position, Random.rotation) as GameObject;
+            go.GetComponent<Rigidbody>().velocity = transform.forward * velocity;
+            timer -= 1.0f / rate;
+        }
+    }
+}
